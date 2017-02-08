81639ef8b4e222c81b91a7f9a6eefe0c26b0cf84
arhik <karthik.katipalli@gmail.com>
2017-02-08 15:22:16 -0500

    first steps rm

--- Arc.py
+++
@@ -1,9 +0,0 @@
-class Arc:
-    def __init__(self):
-        self.weight = None
-        self.to = None
-        self.frm = None
-        
-    def _validate(self):
-        if self.to is not None:
-            if type(self.to) == type(Transition) 
--- Clock.py
+++
@@ -1,9 +0,0 @@
-class Clock:
-    def __init__(self):
-        self.timeElapsed = 0
-    def reset(self):
-        self.timeElapsed = 0
-    def tick(self):
-        while True:
-            self.timeElapsed = self.timeElapsed + 1
-            yield self.timeElapsed
